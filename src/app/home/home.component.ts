f230caf1b099ba88af1cdc8ae7d219d2f4cd90c7
Ikram Akbar <ikramakbar71@gmail.com>
2022-10-06 19:55:14 +0600

    re align

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,123 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
-
-
-
-// interface for userValidation
 export interface User {
-  userName: string;
-  userAge: number | null;
-  userPhone: number | null;
-  
-  userEmail: string;
-  userWeight: number | null;
-  userHeight: number | null;
+  name: string;
+  age: number | null;
+  phone: number | null;
+  email: string;
+  weight: number | null;
+  height: number | null;
 }
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-  
 export class HomeComponent implements OnInit {
-
-
-  // create an object for input validation
-
-  userValidation: User = {
-    userName: "",
-    userAge: null,
-    userPhone: null,
-    userEmail: "",
-    userWeight: null,
-    userHeight: null,
-   
-
-  }
-
-
+ 
   formData: any = "";
   weight: any;
   height: any;
   calcBMI: any;
 
-
-
   constructor() {
-    this.userValidation = {} as User
 
   }
-
   ngOnInit(): void {
 
   }
-
-
-  // create an Instance for get form values
-
+ 
   userForm = new FormGroup({
-
-    userName: new FormControl(this.userValidation.userName, [
+    userName: new FormControl<String>("", [
       Validators.required,
       Validators.minLength(6),
       Validators.maxLength(40)
     ]),
-
-    userAge: new FormControl(this.userValidation.userAge, [
+    userAge: new FormControl<Number| null>(null, [
       Validators.required,
-
     ]),
-
-    userGender: new FormControl("male"),
-    userPhone: new FormControl(this.userValidation.userPhone, [
-      Validators.required,
+    userGender: new FormControl<String>("male"),
+    userPhone: new FormControl<Number | null>(null,[
       Validators.pattern('(?:\\+88|88)?(01[3-9]\\d{8})')
-
     ]),
-
-    userEmail: new FormControl(this.userValidation.userEmail, [
+    userEmail: new FormControl<String>("", [
       Validators.required,
       Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
     ]),
-
-    userWeight: new FormControl(this.userValidation.userWeight, [
+    userWeight: new FormControl<Number | null>(null, [
       Validators.required,
     ]),
-    userHeight: new FormControl(this.userValidation.userHeight, [
+    userHeight: new FormControl<Number | null>(null, [
       Validators.required,
     ]),
-    
-
   });
-
-
-
-  // Declare a getter method
-  get userName() {
-    return this.userForm.get("userName");
-  }
-  get userAge() {
-    return this.userForm.get("userAge")
-  }
-  get userPhone() {
-    return this.userForm.get("userPhone")
-  }
-  get userEmail() {
-    return this.userForm.get("userEmail")
-  }
-  get userWeight() {
-    return this.userForm.get("userWeight")
-  }
-  get userHeight() {
-    return this.userForm.get("userHeight")
-  }
- 
-
-
-  // create a method for onsubmit form 
-
+  
   onSubmit() {
     this.formData = this.userForm.value;
     console.log(this.formData);
@@ -125,8 +59,8 @@ export class HomeComponent implements OnInit {
     this.height = this.formData.userHeight;
     this.calcBMI = (this.weight / (this.height * 2));
     this.userForm.reset()
-  
-  
+
+
   }
 
 }
